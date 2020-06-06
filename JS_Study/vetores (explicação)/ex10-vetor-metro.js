b5da49f28575cee8dce402fd177da4a0d924abb5
Thiago Ferrari <thiagofcjr@gmail.com>
2020-06-05 22:57:15 -0300

    array complete study

---
+++ JS_Study/vetores (explicação)/ex10-vetor-metro.js
@@ -0,0 +1,44 @@
+      // Liga o modo "chato" do JS
+      // Não deixa usar variáveis sem let ou var
+      'use strict'
+
+      /*
+         O ano é 2119. A prefeita Enza Valentina Faleiros Bittar
+         inaugura a primeira linha do Metrô de Franca, no sentido
+         norte-sul, com as seguintes estações: Santa Teresinha,
+         Estação, Centro, Jardim Lima e Aeroporto I.
+         1) Crie um vetor vazio e depois insira as estações no
+            vetor. Faça um loop for TRADICIONAL para exibir as 
+            estações e depois passe um traço horizontal (<hr>).
+         No ano seguinte, a linha foi expandida com a inauguração
+         das estações City Petrópolis, no extremo norte, e Aeroporto
+         IV, no extremo sul.
+
+         2) Insira as novas estações no vetor nas posições apropriadas.
+            Faça um loop FOR..OF para exibir as estações e depois
+            passe um traço horizontal (<hr>).
+         Já em 2125, foi criada a estação Vila Imperador, entre
+         as estações Santa Teresinha e Estação, para atender à grande
+         demanda da maior universidade da cidade, a Fatec de Franca.
+         Um ano depois, a estação Jardim Alvorada passou a funcionar
+         entre as estações Jardim Lima e Aeroporto I.
+
+         3) Insira as novas estações no vetor nas posições apropriadas.
+            Faça um loop for TRADICIONAL EM SENTIDO INVERSO para exibir
+            as estações e depois passe um traço horizontal (<hr>).
+         Em 2130, a estação Centro foi desativada, e em seu lugar duas
+         novas estações entraram em funcionamento: as estações 9 de Julho
+         e Champagnat.
+
+         4) Modifique o vetor de acordo com as alterações.
+            Faça um loop for TRADICIONAL para exibir as estações e depois
+            passe um traço horizontal (<hr>).
+         Em 2133, no entanto, Franca entrou em guerra com os 
+         restinguenses, que invadiram e destruíram as duas estações 
+         mais ao sul do metrô de Franca. Neste mesmo ano, em meio
+         à crise, foi fechada a estação Santa Teresinha.
+         
+         5) Modifique o vetor de acordo com as alterações.
+            Faça um loop FOR...OF para exibir as estações e depois
+            passe um traço horizontal (<hr>).            
+      */
