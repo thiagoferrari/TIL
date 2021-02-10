bd93e11651da9dc0734ea94c664585d8fc0c7d5b
Thiago Ferrari <thiagofcjr@gmail.com>
2021-02-10 07:25:58 -0300

    aula faculdade 2021/02/09

---
+++ TIL/000 - Faculdade (college)/ProgramWEB/00 - Revisao-js/template-literals.js
@@ -0,0 +1,13 @@
+let nome = "João"
+let idade = 26
+
+// Mensagem usando concatenação
+console.log('Meu nome é ' + nome + ' e minha idade é ' + idade + '.')
+
+// Mensagem usando TEMPLATE LITERAL
+console.log(`Meu nome é ${nome} e minha idade é ${idade}.`)
+console.log(`Meu nome é ${nome.toUpperCase()} e minha idade é ${idade}.`)
+console.log(`7 vezes 6 é igual a ${7 * 6}.`)
+
+// Tentando usar template literals com outro tipo de aspas
+console.log('Meu nome é ${nome} e minha idade é ${idade}.')
